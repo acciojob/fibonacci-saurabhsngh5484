ed34bee8631eae48593badc2601a6a286a263179
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-04-11 23:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function fibonacci(num) {
 // your code here
-	 if (num === 0) {
-    return 0;
-  } else if (num === 1 || num === 2) {
-    return 1;
-  } else if (num > 50) {
-    return "Number too large, please enter a number between 0 and 50";
-  } else {
-    let fibArr = [0, 1];
-    for (let i = 2; i <= num; i++) {
-      fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
-    }
-    return fibArr[num];
-  }
+	if(num <= 2){
+		return num-1;
+	}
+	
+	let n1 =0; // 1
+	let n2 =1; // 2
+	var next = 0; // 2
+	var number = parseInt(num);
+	for(i =3;i <=number ; i++){
+		next = n1+n2;
+		n1 = n2;
+		n2 = next;
+	
+	}
+	return next;
 }
 
 module.exports = fibonacci;
